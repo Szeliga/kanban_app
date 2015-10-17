f95167a3404656c1152706ac16711328eaa899db
Szymon Szeliga <szymon.szeliga@netguru.co>
2015-10-17 12:51:22 +0200

    Basic note adding

---
+++ app/components/App.jsx
@@ -0,0 +1,37 @@
+import AltContainer from 'alt/AltContainer';
+import React, {Component} from 'react';
+import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
+
+export default class App extends Component {
+  render() {
+    const notes = this.props.notes;
+
+    return (
+      <div>
+        <button className="add-note" onClick={this.addNote}>+</button>
+        <AltContainer
+          stores={[NoteStore]}
+          inject={ {
+            items: () => NoteStore.getState().notes
+          } }
+        >
+          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+        </AltContainer>
+      </div>
+    );
+  }
+
+  addNote() {
+    NoteActions.create({task: 'New task'});
+  }
+
+  editNote(id, task) {
+    NoteActions.update({id, task});
+  }
+
+  deleteNote(id) {
+    NoteActions.delete(id);
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
+
+var TARGET = process.env.npm_lifecycle_event;
+var ROOT_PATH = path.resolve(__dirname);
+
+var common = {
+  entry: path.resolve(ROOT_PATH, 'app'),
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(ROOT_PATH, 'build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['eslint'],
+        include: path.resolve(ROOT_PATH, 'app')
+      }
+    ],
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: path.resolve(ROOT_PATH, 'app')
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
+  ]
+};
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          loaders: ['react-hot', 'babel'],
+          include: path.resolve(ROOT_PATH, 'app')
+        }
+      ]
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
